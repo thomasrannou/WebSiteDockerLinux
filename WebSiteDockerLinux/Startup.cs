1ff5811239474898e6d3f9577679e369e903a4fe
Thomas RANNOU <thomas.rannou@soprasteria.com>
2020-08-19 15:25:27 +0200

    Montée de version .Net Core 3.1

--- WebSiteDockerLinux/Startup.cs
+++ WebSiteDockerLinux/Startup.cs
@@ -30,9 +30,8 @@ namespace WebSiteDockerLinux
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
-
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            //services.AddControllers(options => options.EnableEndpointRouting = false);
+            services.AddMvc(option => option.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
